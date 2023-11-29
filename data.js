fa7a85ef63e0f56a0fdc95edf433a3b56ec8ca53
sky67sml <nextman67@yandex.ru>
2023-11-29 13:01:03 +0300

    upload

---
+++ data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.533776516631855,
+        "pitch": 0.11198031466875236,
+        "fov": 1.396590613327657
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2722416883564325,
+          "pitch": 0.09317692903366215,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7890107233575918,
+          "pitch": 0.0911827264725158,
+          "title": "Cfkjycbr",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.8039648383948492,
+        "pitch": 0.49459191933403446,
+        "fov": 1.396590613327657
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.948752779928533,
+          "pitch": 0.1265574983060418,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 1.8919211236447495,
+          "pitch": 0.1597302659736144,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8443162878102353,
+        "pitch": 0.47320568644225247,
+        "fov": 1.396590613327657
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.118142508599483,
+          "pitch": 0.1733493278238356,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Salon",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
